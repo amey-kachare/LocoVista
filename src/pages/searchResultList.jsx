7c7e600a1f5467eabbdbca9056d0b4ca034d0a9b
padakitanmay <padakitanmay@gmail.com>
2023-10-06 12:48:03 +0530

    V3

--- src/pages/searchResultList.jsx
+++ src/pages/searchResultList.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const SearchResultList = () => {
   return (
+    <>
     <div>
       SearchResultList
     </div>
+    </>
   )
 }
 
--- src/shared/searchbar.jsx
+++ src/shared/searchbar.jsx
@@ -32,21 +32,21 @@ const SearchBar = () => {
                 <span><i class="ri-map-pin-2-line"></i></span>
                 <div>
                     <h6>Location</h6>
-                    <input type="text" placeholder='Where are you going ?' ref={locationRef} />
+                    <input type="text" placeholder=' Where are you going ?' ref={locationRef} />
                 </div>
             </FormGroup>
             <FormGroup className='d-flex gap-3 form_group form_group-fast'>
                 <span><i class="ri-pin-distance-line"></i></span>
                 <div>
                     <h6>Distance</h6>
-                    <input type="number" placeholder='Distance k/m' ref={distancceRef} />
+                    <input type="number" placeholder=' Distance km' ref={distancceRef} />
                 </div>
             </FormGroup>
             <FormGroup className='d-flex gap-3 form_group form_group-last'>
                 <span><i class="ri-group-line"></i></span>
                 <div>
                     <h6>Max People :</h6>
-                    <input type="number" placeholder='0' ref={maxGroupSizeRef} />
+                    <input type="number" placeholder=' 0' ref={maxGroupSizeRef} />
                 </div>
             </FormGroup>
             <span className='search_icon' type='submit' onClick={handleClick}><i class="ri-search-line"></i></span>
