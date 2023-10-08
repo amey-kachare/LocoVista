07a6c5e56e7ba2972786ea73cb9d927314402a99
Amey0008 <ameykachare1234@gmail.com>
2023-10-08 15:36:04 +0530

    oh yeah

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -80,7 +80,7 @@ const Header = () => {
              <div className="nav_btns d-flex align-items-center gap-4">
               {
                 user?(<>
-                <h5 className='mb-0'>{user}</h5>
+                <h5 className='mb-0'>{user.username}</h5>
                 <Button className='btn btn-dark' onClick={logout}>Logout</Button>
                 </>):
                 (<>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,12 +5,15 @@ import { Link } from 'react-router-dom';
 import loginImg from '../assets/images/login.jpg';
 import userIcon from '../assets/images/user.png';
 import { BASE_URL } from '../utills/config';
-import { dispatch } from '../components/context/AuthContext';
+import { useAuth } from '../components/context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const { dispatch } = useAuth();
+  const navigate = useNavigate();
   const [credentials, setCredentials] = useState({
-    email: undefined,
-    password: undefined,
+    email: '',
+    password: '',
   });
 
   const handleChange = (e) => {
@@ -18,74 +21,72 @@ const Login = () => {
   };
 
   const handleClick = async (e) => {
-  e.preventDefault();
-  try {
-    const res = await fetch(`${BASE_URL}/auth/login`, {
-      method: 'post',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    });
+    e.preventDefault();
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+      });
+      
+      const result = await res.json();
+      console.log(result.data)
 
-    const result = await res.json();
+      if (!res.ok) {
+        alert(result.message);
+        // Handle login failure (display error message, etc.)
+      } else {
+        // Assuming the backend returns a user object upon successful login
+        const user =result.data;
+        console.log(user)
+        // Dispatch the user information to your AuthContext
+        dispatch({ type: 'LOGIN_SUCCESS', payload: JSON.stringify(user) });
 
-    if (!res.ok) {
-      alert(result.message);
-      // Handle login failure (display error message, etc.)
-    } else {
-      // Assuming the backend returns a user object upon successful login
-      const { user } = result.data;
-
-      // Dispatch the user information to your AuthContext
-      dispatch({ type: 'LOGIN_SUCCESS', payload: user });
-
-      // Navigate to the desired page after successful login
-      navigate('/dashboard');
+        // Navigate to the desired page after successful login
+        navigate('/home');
+      }
+    } catch (err) {
+      console.error(err);
+      alert('An error occurred while logging in.');
     }
-  } catch (err) {
-    console.error(err);
-    alert('An error occurred while logging in.');
-  }
-};
-
+  };
 
   return (
-    <>
-      <section>
-        <Container>
-          <Row>
-            <Col lg='8' className='m-auto'>
-              <div className='loginContainer d-flex justify-content-between'>
-                <div className='loginImg'>
-                  <img src={loginImg} alt='' />
-                </div>
-                <div className='loginForm'>
-                  <div className='user'>
-                    <img src={userIcon} alt='' />
-                  </div>
-                  <h2>Login</h2>
-                  <Form onSubmit={handleClick}>
-                    <FormGroup>
-                      <input type='email' placeholder='Email' required id='email' onChange={handleChange} />
-                    </FormGroup>
-                    <FormGroup>
-                      <input type='password' placeholder='Password' required id='password' onChange={handleChange} />
-                    </FormGroup>
-                    <Button className='btn secondary_btn auth_btn' type='submit'>
-                      Login
-                    </Button>
-                  </Form>
-                  <p>
-                    Don't have an account?<Link to='/register'>Create</Link>
-                  </p>
-                </div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='8' className='m-auto'>
+            <div className='loginContainer d-flex justify-content-between'>
+              <div className='loginImg'>
+                <img src={loginImg} alt='' />
               </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </>
+              <div className='loginForm'>
+                <div className='user'>
+                  <img src={userIcon} alt='' />
+                </div>
+                <h2>Login</h2>
+                <Form onSubmit={handleClick}>
+                  <FormGroup>
+                    <input type='email' placeholder='Email' required id='email' onChange={handleChange} />
+                  </FormGroup>
+                  <FormGroup>
+                    <input type='password' placeholder='Password' required id='password' onChange={handleChange} />
+                  </FormGroup>
+                  <Button className='btn secondary_btn auth_btn' type='submit'>
+                    Login
+                  </Button>
+                </Form>
+                <p>
+                  Don't have an account? <Link to='/register'>Create</Link>
+                </p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
